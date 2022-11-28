40a4f7d737b4e4db984cb76de2d4e01a073989bb
Sunying RONG <sunying.rong@hotmail.com>
2022-11-28 19:40:22 +0100

    landing page and location

--- app/src/main/java/com/example/jobsrendu/MainActivity.kt
+++ app/src/main/java/com/example/jobsrendu/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.jobsrendu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.*
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.IOException
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvGpsLocation: TextView
+    private val locationPermissionCode = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getLocation()
     }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+    }
+    override fun onLocationChanged(location: Location) {
+        tvGpsLocation = findViewById(R.id.tv_countryName)
+        tvGpsLocation.text = getCountryFromCoordinate(location.latitude, location.longitude)
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun getCountryFromCoordinate(latitude: Double, longitude: Double): String {
+        var geocoder = Geocoder(getApplicationContext(), Locale.getDefault())
+        var country: String = ""
+        try {
+            val listAddresses: List<Address> = geocoder.getFromLocation(latitude, longitude, 1)
+            if (null != listAddresses && listAddresses.size > 0) {
+                country = listAddresses[0].countryName
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return country;
+    }
+
+
 }
