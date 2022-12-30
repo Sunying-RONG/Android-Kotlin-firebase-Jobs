3e87bd2b7c27486552fd1e2fcb5ab31302350d00
Sunying RONG <sunying.rong@hotmail.com>
2022-12-30 17:23:27 +0100

    spinner role selection

--- app/src/main/java/com/example/jobsrendu/NavigationFragment.kt
+++ app/src/main/java/com/example/jobsrendu/NavigationFragment.kt
@@ -1,10 +1,6 @@
 package com.example.jobsrendu
 
-import android.Manifest
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.*
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,16 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import java.io.IOException
-import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
+var country_name = "Country"
 /**
  * A simple [Fragment] subclass.
  * Use the [NavigationFragment.newInstance] factory method to
@@ -49,7 +41,9 @@ class NavigationFragment : Fragment(), AdapterView.OnItemSelectedListener{
         val view: View = inflater.inflate(R.layout.fragment_navigation, container, false)
         tvGpsLocation = view.findViewById<TextView>(R.id.tv_countryName)
         val bundle = arguments
-        val country_name = bundle?.getString("country_name").toString()
+        if (!bundle?.getString("country_name").toString().equals("null")) {
+            country_name = bundle?.getString("country_name").toString()
+        }
         tvGpsLocation.text = country_name
 
         val spinner: Spinner = view.findViewById(R.id.roles_spinner)
@@ -64,6 +58,7 @@ class NavigationFragment : Fragment(), AdapterView.OnItemSelectedListener{
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             // Apply the adapter to the spinner
             spinner.adapter = adapter
+
         }
         return view
     }
@@ -89,17 +84,35 @@ class NavigationFragment : Fragment(), AdapterView.OnItemSelectedListener{
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-//        val intent = Intent(this, NewActivity::class.java)
-//        startActivity(intent)
+        var formerSelect = 0;
+        val intent = Intent(requireContext(), SigninActivity::class.java)
+
         when (p2) {
-            0 -> Log.d("select","Job seeker")
-            1 -> Log.d("select","Employer")
-            2 -> Log.d("select","Agency")
-            3 -> Log.d("select","Administrator")
+            1 -> {
+                intent.putExtra("role", "Job seeker")
+                Log.d("select","Job seeker")
+            }
+            2 -> {
+                intent.putExtra("role", "Employer")
+                Log.d("select","Employer")
+            }
+            3 -> {
+                intent.putExtra("role", "Agency")
+                Log.d("select","Agency")
+            }
+            4 -> {
+                intent.putExtra("role", "Administrator")
+                Log.d("select","Administrator")
+            }
         }
+        if (formerSelect != p2) {
+            startActivity(intent)
+        }
+        formerSelect = p2
+        Log.d("formerSelect", formerSelect.toString())
     }
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
-        Log.d("select nothing", "on select nothing")
+        TODO("Not yet implemented")
     }
 }
