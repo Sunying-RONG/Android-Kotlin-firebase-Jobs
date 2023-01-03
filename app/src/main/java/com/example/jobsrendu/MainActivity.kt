7466fe59ad96765799f09630addd492140db6d9b
Sunying RONG <sunying.rong@hotmail.com>
2023-01-03 20:59:38 +0100

    search job

--- app/src/main/java/com/example/jobsrendu/MainActivity.kt
+++ app/src/main/java/com/example/jobsrendu/MainActivity.kt
@@ -2,20 +2,24 @@ package com.example.jobsrendu
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.*
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.io.IOException
 import java.util.*
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity(), LocationListener {
     private lateinit var locationManager: LocationManager
@@ -23,6 +27,10 @@ class MainActivity : AppCompatActivity(), LocationListener {
     private val locationPermissionCode = 2
     val navigationFragment = NavigationFragment()
     val mBundle = Bundle()
+    val db = Firebase.firestore
+    var employerList = HashMap<String, String>()
+    var agencyList = HashMap<String, String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +38,13 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
         mBundle.putString("country_name", "Country")
         getLocation()
-//        initializeCloudFirestore()
+        getEmployerName()
+        getAgencyName()
+
+        val btn_go = findViewById<Button>(R.id.go)
+        btn_go.setOnClickListener {
+            searchJobs()
+        }
     }
 
     private fun getLocation() {
@@ -74,24 +88,79 @@ class MainActivity : AppCompatActivity(), LocationListener {
         return country;
     }
 
-//    fun initializeCloudFirestore() {
-//        val db = Firebase.firestore
-//        // Create a new user with a first and last name
-//        val user = hashMapOf(
-//            "first" to "Ada",
-//            "last" to "Lovelace",
-//            "born" to 1815
-//        )
-//// Add a new document with a generated ID
-//        db.collection("users")
-//        .add(user)
-//        .addOnSuccessListener { documentReference ->
-//            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-//        }
-//        .addOnFailureListener { e ->
-//            Log.w(TAG, "Error adding document", e)
-//        }
-//
-//    }
+    fun getEmployerName() {
+        db.collection("Employer")
+            .get()
+            .addOnCompleteListener {
+                task ->
+                run {
+                    val documents = task.result
+                    for (document : QueryDocumentSnapshot in documents) {
+                        employerList.put(document.id, document.get("user name") as String)
+                    }
+                }
+            }
+    }
+
+    fun getAgencyName() {
+        db.collection("Agency")
+            .get()
+            .addOnCompleteListener {
+                    task ->
+                run {
+                    val documents = task.result
+                    for (document : QueryDocumentSnapshot in documents) {
+                        agencyList.put(document.id, document.get("user name") as String)
+                    }
+                }
+            }
+    }
+
+    fun searchJobs() {
+        val what = findViewById<EditText>(R.id.what).text.toString()
+        val where = findViewById<EditText>(R.id.where).text.toString()
+        val job_list_container = findViewById<LinearLayout>(R.id.job_list_container)
+        job_list_container.removeAllViews()
+        var view: View
+
+        db.collection("Jobs")
+            .get()
+            .addOnCompleteListener {
+                    task ->
+                run {
+                    val documents = task.result
+                    if (documents.size() == 0) {
+                        Toast.makeText(this, "Wrong email or password !", Toast.LENGTH_SHORT).show()
+                    }
+                    for (document : QueryDocumentSnapshot in documents) {
+                        if ((document.get("job field").toString().contains(what) ||
+                            document.get("job title").toString().contains(what)) &&
+                            document.get("city").toString().contains(where)
+                        ) {
+                            view = LayoutInflater.from(this).inflate(R.layout.createdjob, null)
+                            view.findViewById<TextView>(R.id.created_title).text = document.get("job title") as String
+                            if ((document.get("role") as String).equals("Employer")) {
+                                view.findViewById<TextView>(R.id.provider).text = employerList.get(document.get("provider id"))
+                            } else if ((document.get("role") as String).equals("Agency")) {
+                                view.findViewById<TextView>(R.id.provider).text = agencyList.get(document.get("provider id"))
+                            }
+                            view.findViewById<TextView>(R.id.created_city).text = document.get("city") as String
+                            view.findViewById<TextView>(R.id.created_description).text = document.get("description") as String
+//                            view.setOnClickListener {
+//                                val intentToSignUp = Intent(this, SignupActivity::class.java)
+//                                intentToSignUp.putExtra("role", role)
+//                                intentToSignUp.putExtra("plan", document.id)
+//                                startActivity(intentToSignUp)
+//                            }
+                            job_list_container.addView(view)
+                        }
+                    }
+
+                }
+
+            }
+
+
+    }
 
 }
